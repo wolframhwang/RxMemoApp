db683aae8b46e6d44f7f9a3b6964c137a8c97001
jiwoong <hzw94@naver.com>
2021-05-17 00:13:27 +0900

    Action Binding

--- RxMemoApp/ViewController/MemoComposeViewController.swift
+++ RxMemoApp/ViewController/MemoComposeViewController.swift
@@ -6,18 +6,59 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
+import Action
+import NSObject_Rx
 
 class MemoComposeViewController: UIViewController,ViewModelBindableType {
     var viewModel: MemoComposeViewModel!
     
+    
+    @IBOutlet weak var cancelButton: UIBarButtonItem!
+    @IBOutlet weak var saveButton: UIBarButtonItem!
+    
+    @IBOutlet weak var contextView: UITextView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
     }
     
     func bindViewModel() {
+        //네비게이션 타이틀바인딩하기
+        viewModel.title
+            .drive(navigationItem.rx.title)
+            .disposed(by: rx.disposeBag)
         
+        viewModel.initialText
+            .drive(contextView.rx.text)
+            .disposed(by: rx.disposeBag)
+        
+        cancelButton.rx.action = viewModel.cancelAction//action 은 action에 바인딩하면됌!
+        //Tap 하고! double탭 막을려고 쓰로틀링해야해
+        saveButton.rx.tap.throttle(.milliseconds(500), scheduler: MainScheduler.instance)
+            .withLatestFrom(contextView.rx.text.orEmpty)
+            .bind(to: viewModel.saveAction.inputs)
+            .disposed(by: rx.disposeBag)
+        
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        contextView.becomeFirstResponder()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        if contextView.isFirstResponder {
+            contextView.resignFirstResponder()
+        }
     }
     /*
     // MARK: - Navigation
