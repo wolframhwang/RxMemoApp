ff056ad1c70b89f226e32081676c4690f7cfabf1
jiwoong <hzw94@naver.com>
2021-05-22 22:58:42 +0900

    ㅇ

--- RxMemoApp/ViewController/MemoComposeViewController.swift
+++ RxMemoApp/ViewController/MemoComposeViewController.swift
@@ -42,7 +42,27 @@ class MemoComposeViewController: UIViewController,ViewModelBindableType {
             .bind(to: viewModel.saveAction.inputs)
             .disposed(by: rx.disposeBag)
         
+        let willShowObervable = NotificationCenter.default.rx.notification(UIResponder.keyboardWillShowNotification)
+            .map { ($0.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue.height ?? 0}
         
+        let willHideObservable =
+            NotificationCenter.default.rx.notification(UIResponder.keyboardWillHideNotification)
+            .map { noti -> CGFloat in 0}
+        let keyboardObservable = Observable.merge(willShowObervable, willHideObservable)
+            .share()
+        
+        keyboardObservable.subscribe(onNext: { [weak self] height in
+            guard let strongSelf = self else { return }
+            var inset = strongSelf.contentTextView.contentInset
+            inset.bottom = height
+            
+            var scrollInset = strongSelf.contentTextView.scrollIndicatorInsets
+            scrollInset.bottom = height
+            UIView.animate(withDuration: 0.3) {
+                strongSelf.contentTextView.contentInset = inset
+                strongSelf.contentTextView.scrollIndicatorInsets = scrollInset
+            }
+        }).disposed(by: rx.disposeBag)
     }
     
     override func viewWillAppear(_ animated: Bool) {
